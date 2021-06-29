f08f0a56beca912e86637d2fe7ed644a6bbde4c4
Agust <agusterlingsson@gmail.com>
2021-06-29 14:36:12 -0500

    final commit with final code

--- app.js
+++ app.js
@@ -30,13 +30,17 @@ var chartGroup = svg.append("g")
       data.poverty = +data.poverty;
       data.healthcare = +data.healthcare;
     });
-
+//Create x scale
     var xLinearScale = d3.scaleLinear()
-    .domain([20, d3.max(scaleData, d => d.poverty)])
+    .domain([8, d3.max(scaleData,function(d){
+    return +d.poverty;
+    })])
     .range([0, width]);
-
-  var yLinearScale = d3.scaleLinear()
-    .domain([0, d3.max(scaleData, d => d.healthcare)])
+//create y scale
+    var yLinearScale = d3.scaleLinear()
+    .domain([2, d3.max(scaleData,function(d){
+    return +d.healthcare;
+    })])
     .range([height, 0]);
 
  //  Create axis functions
@@ -44,39 +48,83 @@ var chartGroup = svg.append("g")
     let leftAxis = d3.axisLeft(yLinearScale);
 
     //let yLinearScale = yScale(stateData, chosenYAxis);
-
+  // append x axis
     let xAxis = chartGroup.append("g")
     .attr("transform", `translate(0, ${height})`)
     .call(bottomAxis);
-
-     let yAxis = chartGroup.append("g")
-     .call(leftAxis);
-
-
+//append y axis
+        var yAxis = chartGroup.append('g')
+        .classed('y-axis', true)
+        .call(leftAxis);
 
 
 // create circles
     var circlesGroup = chartGroup.selectAll("circle")
-    .data(data)
+    .data(scaleData)
     .enter()
     .append("circle")
-    .attr("cx", (d,i) => xScale(d.poverty))
-    .attr("cy", d => yScale(d.healthcare))
+    .attr("cx", (d,i) => xLinearScale(d.poverty))
+    .attr("cy", d => yLinearScale(d.healthcare))
     .attr("r", "10")
     .attr("fill", "red")
     .classed("stateCircle", true)
 
+    var toolTip = d3.tip()
+    .attr("class", "tooltip")
+    .offset([-10, 30])
+    .html(function(d) {
+        return (`${d.state}<br>Poverty: ${d.poverty}<br>Healthcare: ${d.healthcare}`);
+    });
+
+
+    chartGroup.call(toolTip);
+
+    // Step 8: Create event listeners to display and hide the tooltip
+    // ==============================
+    circlesGroup.on("mouseover", function(data) {
+      toolTip.show(data, this);
+    })
+      // onmouseout event
+      .on("mouseout", function(data, index) {
+        toolTip.hide(data);
+      });
+
+
 //Add text
-    var textGroup = chartGroup.selectAll('.stateText')
-      .data(data)
-      .enter()
-      .append('text')
-      .classed('stateText', true)
-      .attr('x', d => xLinearScale(d[chosenXAxis]))
-      .attr('y', d => yLinearScale(d[chosenYAxis]))
-      .attr('dy', 4)
-      .attr('font-size', '12px')
-      .text(function(d){return d.abbr});
+    // var textGroup = chartGroup.selectAll('.stateText')
+    //   .data(scaleData)
+    //   .enter()
+    //   .append('text')
+    //   .classed('stateText', true)
+    //   .attr('x', d => xLinearScale(d[bottomAxis]))
+    //   .attr('y', d => yLinearScale(d[leftAxis]))
+    //   .attr('dy', 4)
+    //   .attr('font-size', '12px')
+    //   .text(function(d){return d.abbr});
+
+    //   var textGroup = chartGroup.selectAll("text")
+    //   .attr("transform", "rotate(-90)")
+    //   .attr("y", d - margin.left)
+    //   .attr("x", d - (height / 2))
+    //   .attr("dy", 4)
+    //   .attr("class", "axisText")
+    //   .text("Poverty Rate");
+
+    var povertyLabel = chartGroup.append("text")
+      .attr("x", 0)
+      .attr("y", 40)
+      .attr("value", "poverty")
+      .attr("transform", `translate(${width / 2}, ${height})`)
+      .text("In Poverty (%)")
+      .classed("active", true);
+
+    var healthcareLabel = chartGroup.append("text")
+      .attr("x", -(height / 2))
+      .attr("y" + 20)
+      .attr("transform", "rotate(-90)")
+      .text("Healthcare")
+      .classed("active", true);
+
 
 
     
